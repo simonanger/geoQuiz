d4c602dc55f2b91d086471e19ddbe2ba304f0d9c
Simon Anger <simonanger89@gmail.com>
2017-10-06 08:40:44 +0100

    First commit

---
+++ GeoQuiz/app/src/main/java/com/example/simonanger/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.simonanger.geoquiz;
+
+/**
+ * Created by simonanger on 02/10/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
